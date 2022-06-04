0fc0020936c03f934f4a291ca1b1b47e123b8e98
Juan Carlos Castellanos Figueira <97990311+Jcastell16@users.noreply.github.com>
2022-06-03 21:16:37 -0400

    Update test.js
    Ajuste

--- test.js
+++ test.js
@@ -1,7 +1,34 @@
+const { sum } = require('./app.js');
+
+test('adds 14 + 9 to equal 23', () => {
+    //dentro de la prueba llamamos a nuestra función sum con 2 números
+    let total = sum(14, 9);
+
+    // esperamos que la suma de esos 2 números sea 23
+    expect(total).toBe(23);
+});
+
 test("One euro should be 1.206 dollars", function(){
     // importo la funcion desde app.js
     const { fromEuroToDollar } = require('./app.js')
     
     // hago mi comparacion (la prueba)
     expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
-})
+});
+
+
+test("One dolar should be 107 yenes", function(){
+    // importo la funcion desde app.js
+    const { fromDollarToYen } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromDollarToYen(1)).toBe(107);
+});
+
+test("35 yenes should be 3 pounds", function(){
+    // importo la funcion desde app.js
+    const { fromYenToPound } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromYenToPound(450)).toBe(3);
+});
